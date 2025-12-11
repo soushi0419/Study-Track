7f33975fbca285f9620698a55e46b1105a410661
soushi fuzita <st002475@m03.kyoto-kcg.ac.jp>
2025-12-11 10:49:48 +0900

    Add subject-form submit Process to subject.js

--- frontEnd/js/subject.js
+++ frontEnd/js/subject.js
@@ -4,3 +4,37 @@ const { json } = require("body-parser");
 document.addEventListener('DOMContentLoaded', () => {
     loadSubjects();
 });
+
+//教科フォーム送信時の処理
+document.getElementById('subject-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('subject-name').value;
+    const comment = document.getElementById('comment').value;
+
+    try {
+        const response = await fetch('/api/subject', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                comment: comment
+            })
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            alert('教科を登録しました');
+            document.getElementById('subject-form').reset();
+            loadSubjects();
+        } else {
+            alert(data.message);
+        }
+    } catch (error) {
+        console.error('エラー:', error);
+        alert('通信エラーが発生しました。');
+    }
+});
